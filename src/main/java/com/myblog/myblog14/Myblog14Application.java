4f7c31bfa1eab3a67546c2a648cd8ed74faa8f38
tanveer2113 <tanveerhussain2113@gmail.com>
2024-01-16 18:13:01 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog14/Myblog14Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog14/Myblog14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
